9ff543e2c3f23e34cd4b6fc0ee169c44ad2024b3
Aarnav Srivastava <70161949+AarnavSrivastava@users.noreply.github.com>
2024-02-27 11:09:40 -0500

    msg

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,48 @@
-use std::{collections::HashMap, error::Error, result::Result};
-use std::fs;
+use std::{error::Error, result::Result};
+use std::{fs, path::{Path, PathBuf}};
 use std::time::{SystemTime, Duration};
+use fasttext::{Args, ModelName, FastText};
 
-fn process_data(text: &str) -> String {
-    let mut result = String::new();
-    for c in text.chars() {
-        if !c.is_ascii_punctuation() {
-            result.push(c);
+fn get_files_in_folder(path: &str) -> Result<Vec<PathBuf>, Box<dyn Error>> {
+    let entries = fs::read_dir(path).expect("Expectedvalid path");
+    let all: Vec<PathBuf> = entries
+        .filter_map(|entry| Some(entry.ok()?.path()))
+        .collect();
+    Ok(all)
+}
+
+fn gen_ftt_word_vectors(paths: &Vec<PathBuf>) -> Result<(), Box<dyn Error>> {
+    let output_folder = Path::new("english_word_vectors");
+
+    for path in paths {
+        let mut output_path = PathBuf::new();
+        output_path.set_file_name(&format!("{}_word_vector.bin", output_folder.join(path.file_name().expect("Expected file name to be unwrapped")).to_str().expect("Expected output path")));
+        if output_path.exists() {
+            continue;
         }
-    }
-    result
-}
 
-fn collect_data(doc: &str) -> Result<HashMap<String, i64>, Box<dyn Error>> {
-    let mut data: HashMap<String, i64> = HashMap::new();
+        let mut ftt = FastText::new();
+        let mut args_ftt = Args::new();
 
-    let contents = fs::read_to_string(doc).expect("File not read :(");
+        args_ftt.set_model(ModelName::CBOW);
+        args_ftt.set_input(&path.to_string_lossy()).expect("Expected valid input");
 
-    let processed_contents = process_data(&contents);
-    
-    let results = processed_contents.split_ascii_whitespace();
+        ftt.train(&args_ftt)?;
 
-    for word in results {
-        data.insert(word.to_string(), if data.get(word) != None { data.get(word).expect("Key Invalid") + 1 } else { 1 });
+        ftt.save_model(output_path.to_str().expect("Expected valid path"))?;
     }
 
-    Ok(data)
-}
-
-fn calculate_weightings(count_words: i64, freqs: HashMap<String, i64>) -> HashMap<String, f64> {
-    let mut dict: HashMap<String, f64> = HashMap::new();
-
-    for key in freqs.keys() {
-        let count = freqs.get(key).unwrap();
-
-        let weight: f64 = ((count - count_words) as f64)/(count_words as f64);
-
-        dict.insert(key.to_string(), weight);
-    }
-
-    dict
+    Ok(())
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
     let t1: SystemTime = SystemTime::now();
 
-    let data = collect_data("/Users/aarnavsrivastava/monolingual-identification/src/training_textfiles/polenglang.txt").unwrap();
-    let dict = calculate_weightings(10, data);
+    let path1 = "/Users/aarnavsrivastava/Desktop/sanskrit-english-identification/english";
 
-    for key in dict.keys() {
-        println!("Word: {}\tWeighting: {}", key.to_string(), dict.get(key).expect("Expected value"));
-    }
+    let paths = get_files_in_folder(&path1)?;
+    
+    gen_ftt_word_vectors(&paths)?;
 
     let elapsed: Duration = t1.elapsed().unwrap();
 
