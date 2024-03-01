ffa91f27f88c5174a931f36a99a7a37719f503a5
Aarnav Srivastava <70161949+AarnavSrivastava@users.noreply.github.com>
2024-03-01 08:00:15 -0500

    working on solution to word vectors containing a lot of information

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use std::{error::Error, result::Result};
 use std::{fs, path::{Path, PathBuf}};
+use clap::Parser;
 use fasttext::{Args, ModelName, FastText};
 
 pub fn get_files_in_folder(path: &str) -> Result<Vec<PathBuf>, Box<dyn Error>> {
@@ -10,15 +11,22 @@ pub fn get_files_in_folder(path: &str) -> Result<Vec<PathBuf>, Box<dyn Error>> {
     Ok(all)
 }
 
-pub fn gen_ftt_word_vectors(paths: &Vec<PathBuf>) -> Result<(), Box<dyn Error>> {
-    let output_folder = Path::new("english_word_vectors");
+pub fn gen_ftt_word_vectors(paths: &Vec<PathBuf>, new_directory: &str) -> Result<(), Box<dyn Error>> {
+    let output_folder = Path::new(new_directory);
+    fs::create_dir_all(output_folder.to_str().expect("Expected valid directory"))?;
 
     for path in paths {
         let mut output_path = PathBuf::new();
-        output_path.set_file_name(&format!("{}_word_vector.bin", output_folder.join(path.file_name().expect("Expected file name to be unwrapped")).to_str().expect("Expected output path")));
+        output_path.set_file_name(&format!("{}_word_vector.bin", output_folder.join(path.file_stem().expect("Expected file name to be unwrapped")).to_str().expect("Expected output path").trim()));
+        
         if output_path.exists() {
             continue;
         }
+
+        if fs::metadata(&path)?.len() < 10 {
+            continue;
+        }
+
 
         let mut ftt = FastText::new();
         let mut args_ftt = Args::new();
@@ -32,4 +40,13 @@ pub fn gen_ftt_word_vectors(paths: &Vec<PathBuf>) -> Result<(), Box<dyn Error>> 
     }
 
     Ok(())
+}
+
+#[derive(Parser, Default, Debug)]
+#[clap(author="Aarnav Srivastava", version, about="Generates word vectors given input data and trains models on classifying languages (specifically Sanskrit/English) for the MITRA Team")]
+pub struct CLIArgs {
+    #[clap(short, long, value_parser = clap::builder::NonEmptyStringValueParser::new())]
+    pub train: bool,
+    #[clap(short, long, value_parser = clap::builder::NonEmptyStringValueParser::new())]
+    pub input_directory: Option<String>,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,23 @@
+use std::path::PathBuf;
 use std::{error::Error, result::Result};
 use std::time::{SystemTime, Duration};
+use clap::Parser;
+use sanskrit_english_identification::CLIArgs;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let t1: SystemTime = SystemTime::now();
+    let t: SystemTime = SystemTime::now();
 
-    let path1 = "/Users/aarnavsrivastava/Desktop/sanskrit-english-identification/english";
+    let args = CLIArgs::parse();
 
-    let paths = sanskrit_english_identification::get_files_in_folder(&path1)?;
-    
-    sanskrit_english_identification::gen_ftt_word_vectors(&paths)?;
+    if args.train {
+        let path = args.input_directory.expect("Expected input directory");
 
-    let elapsed: Duration = t1.elapsed().unwrap();
+        let paths: Vec<PathBuf> = sanskrit_english_identification::get_files_in_folder(&path)?;
+        
+        sanskrit_english_identification::gen_ftt_word_vectors(&paths, &new_path)?;
+    }
+
+    let elapsed: Duration = t.elapsed().expect("Error with elapsed time");
 
     println!("Time elapsed: {}", elapsed.as_secs_f64());
 
